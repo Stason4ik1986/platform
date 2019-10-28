63adf02610f881104afd6d3f7e4e972d81423e27
Stas Cherednichenko <first1986st@gmail.com>
2019-10-28 13:21:01 +0200

    Added responsive design and fonts

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,37 @@ $(document).ready(function () {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 1,
+                items: 2,
             },
             480: {
-                items: 1,
+                items: 2,
+                margin: 10
             },
             1000: {
                 items: 3,
+                margin: 20
             }
         }
+    });
+
+    $('.navigation__hidden-button').click(function (e) {
+        $(this).toggleClass('active');
+        $('.navigation__list-hidden').slideToggle();
+
+        e.preventDefault();
+    });
+
+
+    $(function () {
+        $('.popup-modal').magnificPopup({
+            type: 'inline',
+            removalDelay: 300,
+            showCloseBtn: false,
+            mainClass: 'mfp-fade',
+        });
+        $(document).on('click', '.popup-modal-dismiss', function (e) {
+            e.preventDefault();
+            $.magnificPopup.close();
+        });
     });
 });
